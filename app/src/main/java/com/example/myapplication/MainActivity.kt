d839288584316d5f8e1e65e83070ae83988d0872
bahaarizkall <68571388+bahaarizkalla@users.noreply.github.com>
2020-07-21 21:52:10 -0700

    add code

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText( context this, text:"hello from android", Toast.LENTH_LONG).show()
+
+
     }
 }
